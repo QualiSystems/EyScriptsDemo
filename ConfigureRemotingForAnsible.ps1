8c60ffc82fa96769c3bd5ddc2d5dbf6e8fb5f339
kalsky <yanivka@gmail.com>
2024-02-23 08:58:46 -0600

    set timezone info in registry if it's not there

--- ConfigureRemotingForAnsible.ps1
+++ ConfigureRemotingForAnsible.ps1
@@ -4,3 +4,6 @@ winrm set winrm/config/service/auth '@{Basic="true"}'
 Start-Service WinRM
 set-service WinRM -StartupType Automatic
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled false
+
+If ((Get-Item -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation').GetValue("ActiveTimeBias") -eq $null)
+{ New-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation' -Name "ActiveTimeBias" -Value "480" -Type "DWord" -Force | Out-Null }
